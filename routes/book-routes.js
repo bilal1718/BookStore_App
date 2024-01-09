7cb8db22584774a4e494d0bd3e359bb8713e6fd4
bilal1718 <muhbil171845@gmail.com>
2024-01-09 16:54:10 +0500

    The routes define

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,16 +1,10 @@
 const express=require("express");
 const router=express.Router();
 const Book=require("../models/Book");
-router.get("/",async (req,res,next)=>{
-    let books;
-    try{
-        books=await Book.find();
-    }catch(err){
-        console.log(err);
-    }
-    if(!books){
-        return res.status(404).json({message:"No book found"});
-    }
-    return res.status(200).json({books});
-});
+const booksController=require('../controllers/books-controller');
+router.get("/", booksController.getAllBooks);
+router.post("/", booksController.addBook);
+router.get("/:id", booksController.getBookById);
+router.put("/:id", booksController.updateBook);
+router.delete("/:id", booksController.deleteBook);
 module.exports=router;
