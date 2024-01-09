4cedf54f7dbd00626027a07f463e6e89e9a768b3
bilal1718 <muhbil171845@gmail.com>
2024-01-09 16:51:32 +0500

    The express.json() used to see crud operations

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const router=require("./routes/book-routes");
 const app=express();
+app.use(express.json());
 app.use('/books',router);
 
 mongoose.connect("mongodb+srv://admin:OybCL4f5bRrsNy45@cluster0.osyjcj6.mongodb.net/Book-Store?retryWrites=true&w=majority")
