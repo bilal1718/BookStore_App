8c6e1baba31fd5c03431c00f43a2c57dbb56361c
bilal1718 <muhbil171845@gmail.com>
2024-01-10 17:13:28 +0500

    The root element

---
+++ components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './header'
+
+const Home = () => {
+  return (
+    <>
+    <Header />
+    <div className='flex space-x-4 bg-blue-400' >
+        Welcome to Home!
+    </div>
+    </>
+  )
+}
+
+export default Home
