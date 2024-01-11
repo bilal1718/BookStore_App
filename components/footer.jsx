e7ce75a155f7d0101b0d09c2490fcff02768a74b
bilal1718 <muhbil171845@gmail.com>
2024-01-11 21:29:47 +0500

    Make footer for web app

---
+++ components/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-12">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <p className="text-xl font-bold">Book Store</p>
+          <p className="text-lg">© 2024 All Rights Reserved</p>
+        </div>
+        <div>
+          <p className="text-lg">Contact: muhbil171845@gmail.com</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
